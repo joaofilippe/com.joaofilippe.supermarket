dc5fccbe95a8f6b141cf4c795d062efa4b30043a
João Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2022-05-05 13:43:32 -0300

    initial config

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public void Product(){
+        final String NAME;
+        final double PRICE;
+        final int QUANTITY;
+
+        constructor(){
+
+        }
+    }
+}
